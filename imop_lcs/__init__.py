7933b50e52ba527e50d3de384a9cb879879e500c
김동준 <keithape73@gmail.com>
2025-07-27 17:05:32 +0900

    Update __init__.py

--- imop_lcs/__init__.py
+++ imop_lcs/__init__.py
@@ -6,6 +6,8 @@ Description:
     First known O(N log N) algorithm to solve Longest Common Subsequence (LCS)
     using index mapping and LIS (Longest Increasing Subsequence).
 """
+__version__ = "1.0.0"
+__author__ = "Dongjoon Kim (keithape73)"
 
 from .imop_lcs import imop_lcs
 
